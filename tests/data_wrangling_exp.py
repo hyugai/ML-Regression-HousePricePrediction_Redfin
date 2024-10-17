88285b6f9034c1ab29d463ff17f7313da10a6be9
hyugai <huyle210504@gmail.com>
2024-10-17 10:31:26 +0700

    m

--- tests/data_wrangling_exp.py
+++ tests/data_wrangling_exp.py
@@ -13,7 +13,7 @@ df_api = pd.read_csv('resource/data/api.csv', dtype={'sold_date': 'object'})
 summary_file_path = cwd + "/resource/output/logs/dw.txt"
 columns_to_drop = ['address', 'sale_type', 'sold_date', 'days_on_market', 
                    'status', 'next_open_house_start_time', 'next_open_house_end_time', 'url', 
-                   'source', 'mls', 'favorite', 'interested']
+                   'source', 'mls', 'favorite', 'interested', 'location']
 with open(summary_file_path, 'w+') as f:
     f.write('')
 
@@ -25,6 +25,9 @@ df_api_cleaned = df_api.pipe(get_data_summary, "Overviews of raws data from API"
 df_html = pd.read_csv('resource/data/html.csv')
 renamed_columns = {'streetLine': 'address', 'hoa': 'hoa_per_month', 'sqFt': 'square_feet', 'pricePerSqFt': 'dollars_per_square_feet', 
                    'lotSize': 'lot_size', 'yearBuilt': 'year_built', 'propertyType': 'property_type'}
+columns_to_drop = ['address', 'countryCode', 'postalCode']
 df_html_cleaned = df_html.rename(columns=renamed_columns)\
         .pipe(get_data_summary, "Overviews of raws data from HTML", summary_file_path)\
-            .pipe(preprocess_category, summary_file_path)
+            .pipe(preprocess_category, summary_file_path)\
+                .pipe(preprocess, summary_file_path, columns_to_drop)\
+                    .pipe(get_data_summary, "Overviews of cleaned data from HTML", summary_file_path)
